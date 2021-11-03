0c64ce9d8d0a8f51f6b68afecb52adfae21ec99b
tkna <take70qui@gmail.com>
2021-11-03 06:19:50 +0000

    add CreateMinoInstance logic to game

--- game/server.go
+++ game/server.go
@@ -2,8 +2,12 @@ package main
 
 import (
         "bytes"
+        "io/ioutil"
+        "encoding/json"
+        "fmt"
         "net/http"
         "strconv"
+//        "time"
 
         "github.com/labstack/echo"
 )
@@ -13,6 +17,26 @@ type Game struct {
         Width   int     `json:"width"`
         Height  int     `json:"height"`
         Status  string  `json:"status"`
+}
+
+type MinoInstance struct {
+        Id int                  `json:"id"`
+        MinoId int              `json:"mino_id`
+        X int                   `json:"x"`
+        Y int                   `json:"y"`
+        Coords []Coordinate     `json:"coords"`
+        ColorId int             `json:"color_id`
+}
+
+type Coordinate struct {
+        X int   `json:"x"`
+        Y int   `json:"y"`
+}
+
+type CreateMinoInstanceResponse struct {
+        Result string   `json:"result"`
+        Message string  `json:"message`
+        Instance MinoInstance   `json:"instance"`
 }
 
 var games []Game
@@ -39,6 +63,17 @@ func newGame(c echo.Context) error {
                 return err
         }
 
+        instance, err := createMinoInstance()
+        if err != nil {
+                return err
+        }
+        if instance != nil {
+                fmt.Println("success")
+                fmt.Println(instance)
+        } else {
+                fmt.Println("failed")
+        }
+
         return c.JSON(http.StatusOK, g)
 }
 
@@ -52,3 +87,43 @@ func createField(width int, height int) error {
         defer res.Body.Close()
         return err
 }
+
+func createMinoInstance() (*MinoInstance, error) {
+        URL := "http://mino/instances"
+        jsn := ""
+        res, err := http.Post(URL, "application/json", bytes.NewBuffer([]byte(jsn)))
+        if err != nil {
+                return nil, err
+        }
+        defer res.Body.Close()
+
+        body, _ := ioutil.ReadAll(res.Body)
+        var resp CreateMinoInstanceResponse
+        json.Unmarshal(body, &resp)
+
+        if resp.Result == "success" {
+                return &resp.Instance, err
+        }
+        if resp.Result == "failed" {
+                if resp.Message == "collision" {
+                        return nil, err
+                }
+        }
+
+        return nil, err
+}
+
+/*func down() error {
+
+}
+
+func mainLoop() error {
+        instance, err := createMinoInstance()
+        if err != nil {
+                return err
+        }
+        if instance != nil {
+
+                time.Sleep(time.Second * 1)
+        }
+}*/
